fa12fa90c665bacc4dfae981ab38e5cb396c3300
Robert Real <realrobert120@gmail.com>
2022-08-18 19:34:49 +0800

    menu

--- src/app/core/_constants/menu.ts
+++ src/app/core/_constants/menu.ts
@@ -8,15 +8,15 @@ export const superAdminMenuItems = [
 ];
 
 export const adminMenuItems = [
-  new Menu(1, 'Dashboard', '/app/dashboard/v1', '', 'pie_chart', '', false, 0, false, true),
-  new Menu(2, 'Applications', '/app/applications/list', '', 'account_balance', '', false, 0, false, true),
-  new Menu(3, 'My Companies', '/app/companies', '', 'house', '', false, 0, false, true),
+  new Menu(1, 'Dashboard', '/app/dashboard', '', '', '', false, 0, false, true),
+  new Menu(2, 'Users', '/app/users', '', '', '', false, 0, false, true),
+  new Menu(3, 'Products', '/app/products', '', '', '', false, 0, false, true),
+  new Menu(4, 'Packages', '/app/packages', '', '', '', false, 0, false, true),
 ];
 
 export const resellerMenuItems = [
-  new Menu(100, 'Dashboard', '/app/dashboard/v1', '', 'pie_chart', '', false, 0, false, true),
-  new Menu(200, 'Applications', '/app/applications/list', '', 'account_balance', '', false, 0, false, true),
-  new Menu(300, 'Enquirers', '/app/enquirers/list', '', 'follow_the_signs', '', false, 0, false, true),
+  new Menu(1, 'Dashboard', '/app/dashboard', '', '', '', false, 0, false, true),
+  new Menu(5, 'Product Code', '/app/product-code', '', '', '', false, 0, false, true),
 ];
 
 export const userMenuItems = [
