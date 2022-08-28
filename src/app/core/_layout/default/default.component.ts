2aac5f5871af926af097d75667e09479baf6b15e
Robert Real <robert.real@finxp.com>
2022-08-28 21:06:16 +0800

    navigation toggle

--- src/app/core/_layout/default/default.component.ts
+++ src/app/core/_layout/default/default.component.ts
@@ -1,9 +1,10 @@
-import { Component, ChangeDetectorRef, OnDestroy, HostListener, OnInit } from '@angular/core';
+import { Component, ChangeDetectorRef, OnDestroy, HostListener, OnInit, Inject } from '@angular/core';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { Title } from '@angular/platform-browser';
 import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
 import { Menu } from '@core/_models';
 import { MenuService } from '@core/_services';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'ngp-default-layout',
@@ -23,6 +24,7 @@ export class DefaultComponent implements OnInit, OnDestroy {
   private _mobileQueryListener: () => void;
 
   public menuItems: Menu[] = [];
+  public doc: any;
 
   @HostListener('window:resize', ['$event'])
   /* eslint-disable @typescript-eslint/no-unused-vars */
@@ -36,6 +38,7 @@ export class DefaultComponent implements OnInit, OnDestroy {
   }
 
   constructor(
+    @Inject(DOCUMENT) document: Document,
     private _changeDetectorRef: ChangeDetectorRef,
     private _media: MediaMatcher,
     private _title: Title,
@@ -44,6 +47,7 @@ export class DefaultComponent implements OnInit, OnDestroy {
     this.mobileQuery = this._media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => this._changeDetectorRef.detectChanges();
     this.mobileQuery.addEventListener('change', this._mobileQueryListener);
+    this.doc = document;
   }
 
   ngOnInit(): void {
@@ -60,8 +64,9 @@ export class DefaultComponent implements OnInit, OnDestroy {
     this.mobileQuery.removeEventListener('change', this._mobileQueryListener);
   }
 
-  toggle():void
+  toggle(collapseID: any ):void
   {
-
+    this.doc.getElementById(collapseID).classList.toggle("hidden");
+    this.doc.getElementById(collapseID).classList.toggle("block");
   }
 }
