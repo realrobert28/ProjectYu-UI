398b0db2bbacc9f3dd98f55f5cc7fd7523294d57
Robert Real <robert.real@finxp.com>
2022-08-30 12:43:53 +0800

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,28 @@
-/** @type {import('tailwindcss').Config} */
+const path = require('path');
+
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  prefix: '',
+  important: true,
+  purge: {
+    content: [
+      './src/**/*.{html,ts}',
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      minHeight: {
+        '0': '0',
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+        'full': '100%'
+      }
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+  ],
 }
